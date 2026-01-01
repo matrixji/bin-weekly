512622a1e36b46f1d6cbac36c6fdb120f29442e1
Ji Bin <matrixji@live.com>
2026-01-01 21:26:40 +0800

    year 2025
    Signed-off-by: Ji Bin <matrixji@live.com>

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const config = {
         pic: "/default.webp",
         logo: "/favicon.png",
         description: "BIN周刊: 一周一次，分享所见所闻、所思所想",
-        url: "https://w.jibin.net",
+        url: "https://w25.jibin.net",
         github: "https://github.com/matrixji/bin-weekly",
         twitter: "https://twitter.com/matrixji",
         umami: "8451f513-d7e5-4827-a22d-e06f33ba7461",
